4abf245a0d6015e174cdaf9b4b4b46c197c6d919
Меркулов Игорь <47717374+shiseqq@users.noreply.github.com>
2024-09-03 17:26:54 +0700

    Update TestRunner.h

--- include/TestRunner.h
+++ include/TestRunner.h
@@ -14,19 +14,11 @@
 class TestRunner {
 public:
     /**
-     * @brief Constructs a TestRunner object.
-     * @param checker A unique pointer to an IntersectionChecker2D object.
-     */
-    explicit TestRunner(std::unique_ptr<IntersectionChecker2D> checker) : checker(std::move(checker)) {}
-
-    /**
      * @brief Runs tests from the specified file.
      * @param filename The name of the file containing test cases.
      */
     void runTests(const std::string& filename);
 private:
-    std::unique_ptr<IntersectionChecker2D> checker; /**< A unique pointer to an IntersectionChecker2D object */
-
     /**
      * @brief Parses vertices from a string stream.
      * @param ss The string stream containing the vertices data.
