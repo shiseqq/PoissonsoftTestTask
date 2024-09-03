8fe224b5604180a9e37c0b6306a8e8ca4d10552f
Меркулов Игорь <47717374+shiseqq@users.noreply.github.com>
2024-09-03 17:25:55 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,7 @@ int main(int argc, char* argv[]) {
     // checker made for code scalability if you need to add intersection checking for 3D triangles
     // we can add an abstract class IntersectionChecker with a pure virtual method checkIntersection
     // such polymorphism will help us adhere to the Open/Closed Principle
-    auto checker = std::make_unique<IntersectionChecker2D>();
-    TestRunner runner(std::move(checker));
+    TestRunner runner;
     runner.runTests(filename);
 
     return 0;
